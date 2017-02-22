75bcc06d9d7f716c99b995ab8a8d77a8c262d8a3
Tyler Chong <tylrchong@gmail.com>
2017-02-21 14:23:52 -1000

    Serving from 'public' directory

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(process.env.PORT, () => console.log(`💕  Its happening on port ${process.env.PORT || 9696} 💕`));
 
