115f7a5cca89877ca582cfb7596430b5975cec93
Tyler Chong <tylrchong@gmail.com>
2017-02-24 11:51:00 -1000

    Missing <link> and /

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.listen(process.env.PORT, () => console.log(`💕  Its happening on port ${process.env.PORT || 9696} 💕`));
 
