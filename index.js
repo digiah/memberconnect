e89e56ff10831a8fe49f1f2f0d9ee047a3ffde2d
Tyler Chong <tylrchong@gmail.com>
2017-02-27 10:24:05 -1000

    Mongo Driver

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const path = require('path');
 const express = require('express');
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/memberconnect');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error. Database error.'))
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
