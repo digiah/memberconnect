1bb0759c1829136955aaeb82a715d4070691c4dc
Tyler Chong <tylrchong@gmail.com>
2017-03-03 15:18:20 -1000

    Added Parameter

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const app = express();
 
-app.get('/test', (req, res) => {
+app.get('/test/:param', (req, res) => {
 	MongoClient.connect('mongodb://localhost:27017/memberconnect', function (err, db) {
 		if (err) {
 			return console.error('Connection Error. @mongodb');
@@ -20,7 +20,19 @@ app.get('/test', (req, res) => {
 			});
 		}
 		query(function (data) {
-			res.json(data);
+			const valid = [];
+			data.forEach(e => {
+				let found = false;
+				for (let key in e) {
+					if (e[key].includes(req.params.param)) {
+						found = true;
+					}
+				}
+				if (found) {
+					valid.push(e);
+				}
+			});
+			res.json(valid);
 		});
 	});
 });
