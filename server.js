b2818459d456bd7923051b788ba26ac5f4ac5fcf
Tyler Chong <tylrchong@gmail.com>
2017-03-08 10:24:18 -1000

    Commenting what the code is doing

--- server.js
+++ server.js
@@ -26,29 +26,31 @@ app.get('/test/:param?', (req, res) => {
 				data.forEach(e => { // Loops over all the JSON objects in the MongoDB
 					let found = false; // Each Object is initially 'false', based on the parameters given it can be made 'true' and pushed into
 					for (let key in e) { // Loops over all the keys in each individual 'data' object
-						for (let i = 0; i < params.length; i++) {
-							if (params[i].includes('=')) {
-								const keyValue = params[i].split('=');
-								if (e[keyValue[0]] !== null || e[keyValue[0]] !== undefined) {
-									if (e[keyValue[0]].toString().toLowerCase().includes(keyValue[1].toLowerCase())) {
-										found = true;
+						if ({}.hasOwnProperty.call(e, key)) {
+							for (let i = 0; i < params.length; i++) { // Loops over all the parameters passed in the URL
+								if (params[i].includes('=')) {
+									const keyValue = params[i].split('=');
+									if (e[keyValue[0]] !== null || e[keyValue[0]] !== undefined) { // Checks if the key exists in the object
+										if (e[keyValue[0]].toString().toLowerCase().includes(keyValue[1].toLowerCase())) { // Checks if the value exists insie the object key
+											found = true;
+										}
+									}	else { // Returns invalid if the key value is invalid
+										res.json({
+											errorCode: '404',
+											message: `"${keyValue[0]} is not a valid key value."`
+										});
 									}
-								}	else {
-									res.json({
-										errorCode: '404',
-										message: `"${keyValue[0]} is not a valid key value."`
-									});
+								} else if (e[key].toString().toLowerCase().includes(params[i].toLowerCase())) { // If the parameter is not a key value pair, check if the value is anywhere inside the object
+									found = true;
 								}
-							} else if (e[key].toString().toLowerCase().includes(params[i].toLowerCase())) {
-								found = true;
 							}
 						}
 					}
-					if (found) {
+					if (found) { // If the object is 'found' then push it into the array we are returning
 						valid.push(e);
 					}
 				});
-			} else {
+			} else { // Returns all data if no parameters are specified
 				valid = data;
 			}
 			res.json(valid);
