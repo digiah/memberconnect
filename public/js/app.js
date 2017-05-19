2fb810c2ce8d0b98e4b6970cc6e2ce507c379809
Tyler Chong <tylrchong@gmail.com>
2017-05-18 16:21:08 -1000

    Updates

--- public/js/app.js
+++ public/js/app.js
@@ -9,22 +9,9 @@ App.controller('controller', function ($scope, $http) {
 	};
 	$http.get('http://dahi.manoa.hawaii.edu/njs/data')
   .then(function (data) {
-	$scope.data = data.data;
-	$scope.loading = false;
-	function toTitleCase(str) {
-		return str.replace(/\w\S*/g, function (txt) {
-			return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-		});
-	}
-	setTimeout(function () {
-		$('.card-subtitle').each(function (idx, val) {
-			val.innerHTML = toTitleCase(val.innerHTML);
-		});
-		$('.interest-item').each(function (idx, val) {
-			val.innerHTML = toTitleCase(val.innerHTML);
-		});
-	}, 250);
-});
+		$scope.data = data.data;
+		$scope.loading = false;
+	});
 });
 App.controller('queryController', function ($scope) {
 	$scope.pageChangeHandler = function (num) {
