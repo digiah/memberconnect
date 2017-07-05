ba3e7322c1c7f51a09ede695325bab678891a3c0
Tyler Chong <tylrchong@gmail.com>
2017-07-04 22:06:08 -1000

    switch to requests

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const indicative = require('indicative');
 require('dotenv').config();
 const spawn = require('child_process').spawn;
-const $ = require('jquery');
+const request = require('request');
 
 // Synced with the Google Spreadsheet
 require('cron').CronJob({
@@ -30,7 +30,7 @@ app.get('/new', (req, res) => res.sendFile(path.join(__dirname, 'public/new.html
 app.get('/test', (req, res) => res.sendFile(path.join(__dirname, 'public/test.html')));
 app.get('/admin', (req, res) => {
 	if (req.query.ticket) {
-		$.get(`https://authn.hawaii.edu/cas/validate?service=https://dahi.manoa.hawaii.edu/njs/admin&ticket=${req.query.ticket}`, function (data) {
+		request(`https://authn.hawaii.edu/cas/validate?service=https://dahi.manoa.hawaii.edu/njs/admin&ticket=${req.query.ticket}`, function (err, response, data) {
 			console.log(data);
 		});
 		return res.sendFile(path.join(__dirname, 'public/admin.html'))
